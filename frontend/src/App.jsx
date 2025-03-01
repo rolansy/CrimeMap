3a096ff33866f5db80171bb962cfbed293326184
sansidsac <kssreekumar04@gmail.com>
2025-03-01 21:52:18 +0530

    Add routing and folder navigation to the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import NavBar from "./components/NavBar"
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 function App() {
-
   return (
-    <>
+    <Router>
       <NavBar />
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/folder1" element={<div>Folder 1 Content</div>} />
+        <Route path="/folder2" element={<div>Folder 2 Content</div>} />
+        <Route path="/folder3" element={<div>Folder 3 Content</div>} />
+        <Route path="/folder4" element={<div>Folder 4 Content</div>} />
+        <Route path="/folder5" element={<div>Folder 5 Content</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
